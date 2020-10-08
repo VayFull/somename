849510a9bd3ca19639c49b063b21323f53a2905a
VayFull <40316778+VayFull@users.noreply.github.com>
2020-10-08 20:21:17 +0300

    init value

--- src/app/catalog/catalog/components/catalog-page/pages/grid/grid.component.ts
+++ src/app/catalog/catalog/components/catalog-page/pages/grid/grid.component.ts
@@ -9,12 +9,12 @@ import {Product} from '../../../../models/product';
   styleUrls: ['./grid.component.css']
 })
 export class GridComponent implements OnInit {
-  data = new BehaviorSubject<Array<Product>>(new Array<Product>());
   @Input() searchValue: BehaviorSubject<string> = new BehaviorSubject<string>(null);
   backendData: Array<Product> =
     [new Product(1, 'Воппер', 179, 5, 'Вкуснейший бургер'),
       new Product(2, 'Мясо', 200, 4, 'Отличное мясо'),
       new Product(3, 'Хлеб', 30, 5, 'Преподаёт как надо')];
+  data = new BehaviorSubject<Array<Product>>(this.backendData);
 
   constructor() {
   }
@@ -22,7 +22,9 @@ export class GridComponent implements OnInit {
   ngOnInit(): void {
     this.searchValue.pipe(debounceTime(200), distinctUntilChanged())
       .subscribe(data => {
-        this.data.next(this.backendData.filter(x => x.name.includes(data)));
+        if(data!=null){
+          this.data.next(this.backendData.filter(x => x.name.includes(data)));
+        }
       });
 
   }
